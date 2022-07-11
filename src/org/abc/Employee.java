5fcb7387018b83220eed3ca9ffc237823b83ec3e
kaarthi <Kaarthikdhanushka2004@gmail.com>
2022-07-11 11:57:50 +0530

    new code added

---
+++ src/org/abc/Employee.java
@@ -0,0 +1,19 @@
+package org.abc;
+
+public class Employee {
+private void empDetails() {
+System.out.println("This is the empDetails");
+}
+private void empId() {
+System.out.println("EmpId is 222");
+}
+private void empAge() {
+System.out.println("empName is kokila");
+}
+public static void main(String[] args) {
+	Employee e=new Employee();
+	e.empAge();
+	e.empDetails();
+	e.empId();
+}
+}
